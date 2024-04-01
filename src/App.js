b7aa2d8587e4c29fbbed57d016ffb30946e58e3c
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-01 10:02:29 +0100

    added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
  export default function App() {
   return (
     <div className="App">
-      
+     <h1>Weather Aplication</h1>
      
-  <h1>Weather Aplication
-
-  </h1>
-   
+ 
+<footer>
+This project was created by Cristina Mihalachi and is <a href="https://github.com/cristina793/weather-app-react"target="_blank"> open-sourced on GitHub.</a>
+  </footer>
     </div>
   );
 }
