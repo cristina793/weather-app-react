06dc14d9a4db22f5bcd4302d854d0c36d9037f7c
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-01 10:23:58 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
  export default function App() {
   return (
-    <div className="App">
+    <div className="container">
      <h1>Weather Aplication</h1>
      
+     <button className="btn btn-primary">Hello</button>
  
 <footer>
 This project was created by Cristina Mihalachi and is <a href="https://github.com/cristina793/weather-app-react"target="_blank"> open-sourced on GitHub.</a>
