7503992c268dc0953f61858de33c8eee5879900d
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-02 09:31:18 +0100

    Added the basic html

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Weather from "./Weather"
  export default function App() {
   return (
     <div className="container">
-     <h1>Weather Aplication</h1>
+  
      
-     <button className="btn btn-primary">Hello</button>
+   
  <Weather />
 <footer>
 This  project was created by Cristina Mihalachi and is <a href="https://github.com/cristina793/weather-app-react"target="_blank"> open-sourced on GitHub.</a>
