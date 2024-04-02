141c96785603f4c52c06a1d26db5dd5e76a23092
Cristina Mihalachi <avocatmihalachi@gmail.com>
2024-04-02 11:03:57 +0100

    added the css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather"
 
  export default function App() {
   return (
-    <div className="container">
+    <div className=" App container">
   
      
    
--- src/Weather.js
+++ src/Weather.js
@@ -7,23 +7,37 @@ export default function Weather() {
    <div className="Weather">
       <form>
          <div className="row">
-         <div className="col-9">
-         < input 
-         type ="search"
-          placeholder="Enter a city.." 
-          className="form-control"/>
-          </div>
-          <div className="col-3">
-          <input type="submit"  value="Search" className="btn btn-primary"/> 
-     </div> </div></form>
+           <div className="col-9">
+             < input 
+               type ="search"
+               placeholder="Enter a city.." 
+               className="form-control"
+               autoFocus="on"/>
+            </div>
+           <div className="col-3">
+              <input type="submit" 
+               value="Search" 
+               className="btn btn-primary w-100"/> 
+           </div> 
+        </div>
+     </form>
         <h1>Lisbon</h1> 
           <ul>
             <li>Wednesday 07:00</li>
             <li>Mostly Cloudy </li>
-         </ul>
-         <div className="row">
+           </ul>
+         <div className="row mt-3 ">
             <div className="col-6">
-               <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy"/> 6 °C
+              
+                 <img 
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+                   alt="mostly cloudy"
+                  />
+                    
+                    <span className="temperature"> 6 </span>
+                    <span  className="unit" >°C</span>
+                    
+               
             </div>
             <div className="col-6">
                <ul>
@@ -37,9 +51,7 @@ export default function Weather() {
                     Wind: 13 km/h
                   </li>
                </ul>
-             
-            </div>
+             </div>
          </div>
-         
-         </div>)
+   </div>)
 }
